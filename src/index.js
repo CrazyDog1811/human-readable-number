2417e0da947d2e5e03337ef87eeefdda213b5add
CrazyDog1811 <evgeshatertyshnyi@mail.ru>
2022-01-05 11:47:45 +0300

    refactor: change switch-case to elements of array

--- src/index.js
+++ src/index.js
@@ -1,126 +1,16 @@
 module.exports = function toReadable (num) {
-    let strNum = num.toString(10);
-    let readableString = '';
-  
-    if(strNum.length === 1) {
-      readableString = units(strNum);
-    } else if(strNum.length === 2) {
-      readableString = dozens(strNum);
-    } else if(strNum.length === 3) {
-      readableString = `${units(strNum[0])} hundred ${dozens(strNum.slice(1))}`
-    }
-  
-    return readableString.trim() || 'zero';
-}
+  const DIGIT = 'zero,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen,fourteen,fifteen,sixteen,seventeen,eighteen,nineteen';
+  const DOZEN = 'zero,zero,twenty,thirty,forty,fifty,sixty,seventy,eighty,ninety';
 
-function units(char) {
-    let stringUnit = '';
-  
-    switch (char) {
-      case '1':
-        stringUnit = 'one';
-        break;
-      case '2':
-        stringUnit = 'two';
-        break;
-      case '3':
-        stringUnit = 'three';
-        break;
-      case '4':
-        stringUnit = 'four';
-        break;
-      case '5':
-        stringUnit = 'five';
-        break;
-      case '6':
-        stringUnit = 'six';
-        break;
-      case '7':
-        stringUnit = 'seven';
-        break;
-      case '8':
-        stringUnit = 'eight';
-        break;
-      case '9':
-        stringUnit = 'nine';
-        break;
-      default:
-        break;
-    }
-    return stringUnit;
+  const digitArr = DIGIT.split(',');
+  const dozenArr = DOZEN.split(',');
+
+  if(num < 20) {
+    return digitArr[num];
+  } else if(num < 100) {
+    return num % 10 ? `${dozenArr[Math.floor(num / 10)]} ${digitArr[num % 10]}` : `${dozenArr[Math.floor(num / 10)]}`;
+  } else {
+    return `${digitArr[Math.floor(num / 100)]} hundred${ num % 100 ? ' ' + toReadable(num % 100) : ''}`
+    
   }
-  
-  function dozens(str) {
-    let stringUnit = '';
-  
-    if(str[0] === '1') {
-  
-      switch(str[1]) {
-        case '0':
-          stringUnit = 'ten';
-          break;
-        case '1':
-          stringUnit = 'eleven';
-          break;
-        case '2':
-          stringUnit = 'twelve';
-          break;
-        case '3':
-          stringUnit = 'thirteen';
-          break;
-        case '4':
-          stringUnit = 'fourteen';
-          break;
-        case '5':
-          stringUnit = 'fifteen';
-          break;
-        case '6':
-          stringUnit = 'sixteen';
-          break;
-        case '7':
-          stringUnit = 'seventeen';
-          break;
-        case '8':
-          stringUnit = 'eighteen';
-          break;
-        case '9':
-          stringUnit = 'nineteen';
-          break;
-        default:
-          break;
-      } 
-    } else {
-        switch (str[0]) {
-          case '0':
-            stringUnit =  units(str[1]);
-            break;
-          case '2':
-            stringUnit = 'twenty' + ' ' + units(str[1]);
-            break;
-          case '3':
-            stringUnit = 'thirty' + ' ' + units(str[1]);
-            break;
-          case '4':
-            stringUnit = 'forty' + ' ' + units(str[1]);
-            break;
-          case '5':
-            stringUnit = 'fifty' + ' ' + units(str[1]);
-            break;
-          case '6':
-            stringUnit = 'sixty' + ' ' + units(str[1]);
-            break;
-          case '7':
-            stringUnit = 'seventy' + ' ' + units(str[1]);
-            break;
-          case '8':
-            stringUnit = 'eighty' + ' ' + units(str[1]);
-            break;
-          case '9':
-            stringUnit = 'ninety' + ' ' + units(str[1]);
-            break;
-          default:
-            break;
-        }
-      }
-      return stringUnit;
-    }
+}
